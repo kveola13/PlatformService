6426a00b854af590e94ad3f6a6e863cb885d0893
Ola Fredrik Ansnes Kveli <olakveli@gmail.com>
2022-03-14 15:09:05 +0100

    Add project files.

---
+++ PlatformService/Controllers/PlatformsController.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data;
+using PlatformService.DTOS;
+using PlatformService.Models;
+
+namespace PlatformService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        private IPlatformRepo _repository;
+        private IMapper _mapper;
+
+        public PlatformsController(IPlatformRepo platformRepo, IMapper mapper)
+        {
+            _repository = platformRepo;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+        {
+            Console.WriteLine("Getting Platforms..");
+            return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(_repository.GetAllPlatforms()));
+        }
+
+        [HttpGet("{id}", Name = "GetPlatformById")]
+        public ActionResult<PlatformReadDto> GetPlatformById(int id)
+        {
+            if(_repository.GetPlatformById(id) != null) {
+                return Ok(_mapper.Map<PlatformReadDto>(_repository.GetPlatformById(id)));
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public ActionResult<PlatformReadDto> CreatePlatform(PlatformCreateDto platformCreateDto)
+        {
+            var platformModel = _mapper.Map<Platform>(platformCreateDto);
+            _repository.CreatePlatform(platformModel);
+            _repository.SaveChanges();
+            var platformReadDto = _mapper.Map<PlatformReadDto>(platformModel);
+            return CreatedAtRoute(nameof(GetPlatformById), new { Id = platformReadDto.Id }, platformReadDto);
+        }
+    }
+}
---
+++ PlatformService/Data/PlatformRepo.cs
@@ -0,0 +1,43 @@
+﻿using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public class PlatformRepo : IPlatformRepo
+    {
+        private readonly AppDbContext _dbContext;
+
+        public PlatformRepo(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void CreatePlatform(Platform platform)
+        {
+            if(platform == null)
+            {
+                throw new ArgumentNullException(nameof(platform));
+            }
+            _dbContext.Add(platform);
+        }
+
+        public IEnumerable<Platform> GetAllPlatforms()
+        {
+            return _dbContext.Platforms.ToList();
+        }
+
+        public Platform GetPlatformById(int id)
+        {
+            var platform = _dbContext.Platforms.FirstOrDefault(x => x.Id == id);
+            if(platform == null)
+            {
+                throw new Exception("Platform is null");
+            }
+            return platform;
+        }
+
+        public bool SaveChanges()
+        {
+            return (_dbContext.SaveChanges() >= 0);
+        }
+    }
+}
