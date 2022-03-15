4b1e75829cda2b636cd856ba4b77f8c68c2d02e5
Ola Fredrik Ansnes Kveli <olakveli@gmail.com>
2022-03-15 13:43:06 +0100

    Added command data client

---
+++ PlatformService/SyncDataServices/Http/HttpCommandDataClient.cs
@@ -0,0 +1,33 @@
+﻿using PlatformService.DTOS;
+using System.Text.Json;
+using System.Text;
+
+namespace PlatformService.SyncDataServices.Http
+{
+    public class HttpCommandDataClient : ICommandDataClient
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IConfiguration _configuration;
+
+        public HttpCommandDataClient(HttpClient httpClient, IConfiguration configuration)
+        {
+            _httpClient = httpClient;
+            _configuration = configuration;
+        }
+        public async Task SendPlatformToCommand(PlatformReadDto platformReadDto)
+        {
+            var httpContent = new StringContent(
+                JsonSerializer.Serialize(platformReadDto), Encoding.UTF8,mediaType: "application/json"
+                );
+            var response = await _httpClient.PostAsync($"{_configuration["CommandService"]}", httpContent);
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Sync POST to CommandService");
+            }
+            else
+            {
+                Console.WriteLine("Desync POST to CommandService");
+            }
+        }
+    }
+}
