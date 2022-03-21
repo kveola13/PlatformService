798c03222771212f8cfa9e9018da2026031f68fa
Ola Fredrik Ansnes Kveli <olakveli@gmail.com>
2022-03-21 11:27:51 +0100

    Update to startup

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -4,9 +4,9 @@ using PlatformService.SyncDataServices.Http;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
-builder.Services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("InMem"));
+// Add services to the container.
+//builder.Services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("InMem"));
 builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
 builder.Services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
 builder.Services.AddControllers();
@@ -14,6 +14,8 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("PlatformsConn")));
+
 
 var app = builder.Build();
 
@@ -23,6 +25,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+//else
+//{
+//}
+
 Console.WriteLine($"Attempting to connect to {app.Configuration["CommandService"]}");
 
 app.UseHttpsRedirection();
@@ -31,6 +37,6 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-PrepDb.PrepPopulation(app);
+//PrepDb.PrepPopulation(app, app.Environment.IsProduction());
 
 app.Run();
