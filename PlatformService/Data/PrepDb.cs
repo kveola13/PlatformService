c6e2056faa9d8bc81508302492bbb43bd5043ea4
Ola Fredrik Ansnes Kveli <olakveli@gmail.com>
2022-03-21 11:28:14 +0100

    prepping db

--- PlatformService/Data/PrepDb.cs
+++ PlatformService/Data/PrepDb.cs
@@ -1,18 +1,31 @@
-﻿using PlatformService.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using PlatformService.Models;
 
 namespace PlatformService.Data
 {
     public static class PrepDb
     {
-        public static void PrepPopulation(IApplicationBuilder app)
+        public static void PrepPopulation(IApplicationBuilder app, bool isProd)
         {
             using (var ServiceScope = app.ApplicationServices.CreateScope())
             {
-                SeedData(ServiceScope.ServiceProvider.GetService<AppDbContext>());
+                SeedData(ServiceScope.ServiceProvider.GetService<AppDbContext>(), isProd);
             }
         }
-        private static void SeedData(AppDbContext context)
+        private static void SeedData(AppDbContext context, bool isProd)
         {
+            if (isProd)
+            {
+                Console.WriteLine("Applying migration");
+                try
+                {
+                    context.Database.Migrate();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Could not run migrations: {ex.Message}");
+                }
+            }
             if (!context.Platforms.Any())
             {
                 Console.WriteLine("Seeding data..");
